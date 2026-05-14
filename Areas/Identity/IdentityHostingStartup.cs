b1b18dcfe66dd2fec2f53fcb9d1ef6af46c758f3
wateen alrumayh <wamsr2020@gmail.com>
2026-05-14 15:56:56 +0300

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using wateen12148ArticalProject.Data;
+
+[assembly: HostingStartup(typeof(wateen12148ArticalProject.Areas.Identity.IdentityHostingStartup))]
+namespace wateen12148ArticalProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Code/EmailSender.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace wateen12148ArticalProject.Code
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("wateen12148@gmail.com", "pqnbqncgyowggida"),
+                EnableSsl = true,
+            };
+
+            return smtpClient.SendMailAsync("wateen12148@gmail.com", email, subject, htmlMessage);
+        }
+    }
+}
