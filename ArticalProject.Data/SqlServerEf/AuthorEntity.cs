10fd880c1888fbce040ac131ff6370fb86cb5c89
Wateen-ali <148346435+Wateen-ali@users.noreply.github.com>
2026-05-14 16:04:38 +0300

    Add files via upload

---
+++ ArticalProject.Data/SqlServerEf/AuthorEntity.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using wateen12148ArticalProject.Core;
+
+namespace wateen12148ArticalProject.Data.AqlServerEF
+{
+   public class AuthorEntity : IDataHelper<Author>
+    {
+        private DBContext db;
+        private Author _table;
+
+        public AuthorEntity()
+        {
+            db = new DBContext();
+        }
+        public int Add(Author table)
+        {
+            if (db.Database.CanConnect())
+            {
+                db.Author.Add(table);
+                db.SaveChanges();
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int Delete(int Id)
+        {
+            if (db.Database.CanConnect())
+            {
+                _table = Find(Id);
+                db.Author.Remove(_table);
+                db.SaveChanges();
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int Edit(int Id, Author table)
+        {
+            db = new DBContext();
+            if (db.Database.CanConnect())
+            {
+                db.Author.Update(table);
+                db.SaveChanges();
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public Author Find(int Id)
+        {
+            if (db.Database.CanConnect())
+            {
+                return db.Author.Where(x => x.Id == Id).First();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<Author> GetAllData()
+        {
+            if (db.Database.CanConnect())
+            {
+                return db.Author.ToList();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<Author> GetDataByUser(string UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Author> Search(string SerachItem)
+        {
+            if (db.Database.CanConnect())
+            {
+                return db.Author.Where(
+                      x => x.FullName.Contains(SerachItem)
+                      || x.UserId.Contains(SerachItem)
+                      || x.Bio.Contains(SerachItem)
+                      || x.UserName.Contains(SerachItem)
+                      || x.Facbook.Contains(SerachItem)
+                      || x.Twitter.Contains(SerachItem)
+                      || x.Instagram.Contains(SerachItem)
+                      || x.Id.ToString().Contains(SerachItem))
+                   .ToList();
+
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ArticalProject.Data/SqlServerEf/CategoryEnti.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using wateen12148ArticalProject.Core;
+
+namespace wateen12148ArticalProject.Data.AqlServerEF
+{
+   public class CategoryEnti : IDataHelper<Category>
+    {
+        private DBContext db;
+        private Category _table;
+
+        public CategoryEnti()
+        {
+            db = new DBContext();
+        }
+        public int Add(Category table)
+        {
+            if (db.Database.CanConnect())
+            {
+                db.Category.Add(table);
+                db.SaveChanges();
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int Delete(int Id)
+        {
+            if (db.Database.CanConnect())
+            {
+                _table = Find(Id);
+                db.Category.Remove(_table);
+                db.SaveChanges();
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int Edit(int Id, Category table)
+        {
+            db = new DBContext();
+            if (db.Database.CanConnect())
+            {
+                db.Category.Update(table);
+                db.SaveChanges();
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public Category Find(int Id)
+        {
+            if (db.Database.CanConnect())
+            {
+                return db.Category.Where(x => x.Id == Id).First();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<Category> GetAllData()
+        {
+            if (db.Database.CanConnect())
+            {
+                return db.Category.ToList();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public List<Category> GetDataByUser(string UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> Search(string SerachItem)
+        {
+            if (db.Database.CanConnect())
+            {
+                return db.Category.Where(x => x.Name.Contains(SerachItem)
+                || x.Id.ToString().Contains(SerachItem))
+                .ToList();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
